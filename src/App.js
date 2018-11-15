6f99af8059bbff9ea4baa385332df20f23e47892
FreyaNoir <freyanoir@gmail.com>
2018-11-15 03:33:51 +0200

    trainings added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Customerlist from "./components/customerlist";
 import cute from "./cute.png";
+import Traininglist from "./components/traininglist";
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
           <h2>Customer Info</h2>
         </header>
         <Customerlist />
+        <Traininglist />
       </div>
     );
   }
---
+++ src/components/traininglist.jsx
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import ReactTable from "react-table";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+import SaveIcon from "@material-ui/icons/Save";
+import "react-table/react-table.css";
+import * as moment from "moment";
+
+let date = moment().format("11-11-2018");
+
+class Traininglist extends Component {
+  constructor(params) {
+    super(params);
+    this.state = { trainings: [], showSnack: false };
+  }
+
+  componentDidMount() {
+    this.listTrainings();
+  }
+
+  // Get all customers
+  listTrainings = () => {
+    fetch("https://customerrest.herokuapp.com/api/trainings")
+      .then(response => response.json())
+      .then(responseData => {
+        window.responseData = responseData;
+        this.setState({ trainings: responseData.content });
+      });
+  };
+
+  renderEditable = cellInfo => {
+    return (
+      <div
+        style={{ backgroundColor: "#fafafa" }}
+        contentEditable
+        suppressContentEditableWarning
+        onBlur={e => {
+          const data = [...this.state.trainings];
+          data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
+          this.setState({ trainings: data });
+        }}
+        dangerouslySetInnerHTML={{
+          __html: this.state.trainings[cellInfo.index][cellInfo.column.id]
+        }}
+      />
+    );
+  };
+
+  handleClose = () => {
+    this.setState({
+      showSnack: false
+    });
+  };
+
+  render() {
+    const columns = [
+      {
+        Header: "Date",
+        accessor: "date",
+        /* d => {
+          return moment(d.date)
+            .local()
+            .format("DD-MM-YYYY hh:mm:ss a");}, */
+
+        Cell: this.renderEditable
+      },
+      {
+        Header: "Duration",
+        accessor: "duration",
+        Cell: this.renderEditable
+      },
+      {
+        Header: "Activity",
+        accessor: "activity",
+        Cell: this.renderEditable
+      },
+      {
+        Header: "Customer",
+        accessor: "customer",
+        Cell: this.renderEditable
+      }
+    ];
+
+    return (
+      <div>
+        <ReactTable
+          filterable={true}
+          defaultPageSize={10}
+          data={this.state.trainings}
+          columns={columns}
+        />
+      </div>
+    );
+  }
+}
+
+export default Traininglist;
